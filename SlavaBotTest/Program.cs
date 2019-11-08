7269555ec87ea1a292ad22b5668c1c39fa0b6603
Kablukov Yaroslav <yarkablukov@gmail.com>
2019-11-08 22:18:26 +0700

    second commit

--- SlavaBotTest/Program.cs
+++ SlavaBotTest/Program.cs
@@ -15,9 +15,22 @@ namespace SlavaBotTest
     class Program
     {
         private static ITelegramBotClient botClient;
+        private static string botChoice;
+        private static int callbackResult; /*0 - Ничья, 1 - Клиент победил, 2 - Клиент проиграл*/
+        private static Random random;
+        private static object syncObj = new object();
+        private static int GenerateRandomNumber(int from, int to)
+        {
+            lock (syncObj)
+            {
+                if (random == null)
+                    random = new Random(); // Or exception...
+                return random.Next(from, to);
+            }
+        }
         static void Main(string[] args)
         {
-            var proxy = new HttpToSocks5Proxy("188.131.149.30", 1080);
+            var proxy = new HttpToSocks5Proxy("88.247.147.96", 1080);
             var proxyHttpClientHandler = new HttpClientHandler { Proxy = new WebProxy("85.99.228.87:42371"), UseProxy = true };
             var http_lient = new HttpClient(proxyHttpClientHandler);
 
@@ -40,33 +53,94 @@ namespace SlavaBotTest
             if (e.CallbackQuery.Data == null)
                 return;
 
-            var rand = new Random().Next(1,3);
-            /*
-             * 1 - Камень
-             * 2 - Ножницы
-             * 3 - Бумага
-             */
+            int rand = GenerateRandomNumber(1,4);
+            switch (rand) {
+                case 1:
+                    botChoice = "Камень";
+                    break;
+                case 2:
+                    botChoice = "Ножницы";
+                    break;
+                case 3:
+                    botChoice = "Бумага";
+                    break;
+            }
 
             string result = e.CallbackQuery.Data;
 
-            //switch (e.CallbackQuery.Data)
-            //{
-            //    case ("Камень"):
-            //        result = "Камень";
-            //        break;
-            //    case ("Ножницы"):
-            //        result = "Ножницы";
-            //        break;
-            //    case ("Бумага"):
-            //        result = "Бумага";
-            //        break;
-            //    default:
-            //        break;
-            //}
-
             try
             {
-                await botClient.AnswerCallbackQueryAsync(e.CallbackQuery.Id, $"Вы нажали {result}");
+                switch (rand) {
+                    case 1: //Камень
+                        switch (result) {
+                            case "Камень":
+                                callbackResult = 0;
+                                break;
+                            case "Ножницы":
+                                callbackResult = 2;
+                                break;
+                            case "Бумага":
+                                callbackResult = 1;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 2: //Ножницы
+                        switch (result)
+                        {
+                            case "Камень":
+                                callbackResult = 1;
+                                break;
+                            case "Ножницы":
+                                callbackResult = 0;
+                                break;
+                            case "Бумага":
+                                callbackResult = 2;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 3: //Бумага
+                        switch (result)
+                        {
+                            case "Камень":
+                                callbackResult = 2;
+                                break;
+                            case "Ножницы":
+                                callbackResult = 1;
+                                break;
+                            case "Бумага":
+                                callbackResult = 0;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            switch (callbackResult)
+                {
+                    case 0:
+                        await botClient.SendTextMessageAsync(e.CallbackQuery.Message.Chat.Id,
+$"И у меня {botChoice}. Ничья!");
+                        break;
+                    case 1:
+                        await botClient.SendTextMessageAsync(e.CallbackQuery.Message.Chat.Id,
+$"А у меня {botChoice}. Ты победил!");
+                        break;
+                    case 2:
+                        await botClient.SendTextMessageAsync(e.CallbackQuery.Message.Chat.Id,
+$"А у меня {botChoice}. Ты проиграл!");
+                        break;
+                    default:
+                        break;
+                                        }
+
+               // await botClient.Ans(e.CallbackQuery.Id, $"Вы нажали {result}");
                 Console.WriteLine("Сообщение доставлено");
 
             }
@@ -83,10 +157,10 @@ namespace SlavaBotTest
                 return;
             Console.WriteLine("Received Text Message: {0} in chat: {1}", e.Message.Text, e.Message.Chat.Id);
 
-            await botClient.SendTextMessageAsync(
-                e.Message.Chat,
-                $"You said {e.Message.Text}"
-                ).ConfigureAwait(false);
+            //await botClient.SendTextMessageAsync(
+            //    e.Message.Chat,
+            //    $"You said {e.Message.Text}"
+            //    ).ConfigureAwait(false);
 
             switch (e.Message.Text)
             {
